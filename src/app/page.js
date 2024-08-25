2aab4e5aeb329fa70b064b6f11e448b84520bd8b
humayun khan <humayun.n.k@gmail.com>
2024-08-25 13:52:21 -0400

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,88 @@
 import Image from "next/image";
+import Link from "next/link";
+import { ArrowUpRightIcon } from '@heroicons/react/24/solid';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen bg-black text-white p-4 sm:p-6 md:p-8 lg:p-12 flex flex-col items-center">
+      <div className="max-w-2xl w-full px-4 sm:px-6 md:px-8 lg:px-24">
+        <div className="flex justify-center mb-6">
+          <Image
+            src="/avatar.png"
+            alt="Humayun K."
+            width={100}
+            height={100}
+          />
         </div>
-      </div>
+        <h1 className="font-stolen-hand text-4xl sm:text-5xl md:text-6xl mb-6 text-center">HUMAYUN K.</h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <p className="mb-8 text-lg">
+          Hi, I&apos;m Humayun Khan. I&apos;m a designer, startup builder,
+          and writer. I run a design consultancy and product
+          studio called <a href="https://studiomaya.io" className="text-yellow-400 hover:underline">Studio Maya</a>. I&apos;m currently based in
+          Montreal.
+        </p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <section className="mb-8">
+          <h2 className="font-stolen-hand text-3xl sm:text-4xl mb-4">ABOUT</h2>
+          <p className="mb-4 text-lg">
+            For the past ~5 years, I&apos;ve been working as a product
+            design consultant for startups like <a href="https://www.classdojo.com" className="text-yellow-400 hover:underline">ClassDojo</a>,
+            <a href="https://www.ycombinator.com/companies/ruth-health" className="text-yellow-400 hover:underline"> RuthHealth (YC 21)</a> and enterprise companies like <a href="https://agorocarbonalliance.com/" className="text-yellow-400 hover:underline"> Agoro
+            Carbon Alliance</a>, <a href="https://www.appliedmaterials.com/" className="text-yellow-400 hover:underline">Applied Materials</a>, and <a href="https://www.ambrygen.com/" className="text-yellow-400 hover:underline">Ambry Genetics</a>.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="mb-4 text-lg">
+            Alongside my consulting practice, I shipped my first
+            Micro-SaaS product, <a href="https://querycat.co" className="text-yellow-400 hover:underline">QueryCat</a>, which at its peak
+            generated $300MRR, before deciding to upskill and
+            attending a <a href="https://www.lewagon.com/web-development-course" className="text-yellow-400 hover:underline">full-stack web dev bootcamp</a>, where I built
+            <a href="https://www.learnfingo.com" className="text-yellow-400 hover:underline"> Fingo</a>, duolingo for finance. I&apos;m currently tinkering on
+            products in the mental health, Web3, and FinTech space.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <p className="mb-4 text-lg">
+            Previously, I worked as a PM for <a href="https://xello.world" className="text-yellow-400 hover:underline">Xello</a> trying to solve career readiness for students, led design for <a href="https://roadmunk.com" className="text-yellow-400 hover:underline">Roadmunk</a> (acq. by Tempo), and Tiny Hearts (acq. by Shopify).
           </p>
-        </a>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <section className="mb-8">
+          <h2 className="font-stolen-hand text-3xl sm:text-4xl mb-4">WRITING</h2>
+          <p className="mb-4 text-lg">
+            In 2018, I decided to pursue a life-long dream of writing
+            a novel and started working on <a href="https://www.penguinrandomhouse.ca/books/669723/wrong-side-of-the-court-by-hn-khan/9780735270893" className="text-yellow-400 hover:underline">Wrong Side of the
+            Court</a>, with no prior training in writing prose. It was an
+            arduous process and exposed me to the inner workings
+            of the publishing industry. After much perseverance I
+            landed an agent, secured a publishing deal, and was
+            nominated for numerous awards.
           </p>
-        </a>
+          <p className="mb-4 text-lg">
+            I&apos;m currently co-writing the film adaptation alongside
+            the very talented <a href="https://www.arigunnarsson.net/wrongsideofthecourt" className="text-yellow-400 hover:underline">Ari Gunnarsson</a> who&apos;ll be directing the
+            project. I&apos;m also starting a newsletter called Systuhm.
+          </p>
+        </section>
+
+        <section>
+          <h2 className="font-stolen-hand text-3xl sm:text-4xl mb-4">GET IN TOUCH</h2>
+          <p className="mb-4 text-lg">
+            Reach out about design projects, writing, or
+            other interesting ways to work together.
+          </p>
+          <div className="flex space-x-4 mb-8">
+            <Link href="mailto:humayun.n.k@gmail.com" className="text-yellow-400 hover:underline flex items-center" target="_blank" rel="noopener noreferrer">
+              Email
+              <ArrowUpRightIcon className="w-4 h-4 ml-1" />
+            </Link>
+            <Link href="https://twitter.com/humayunk_" className="text-yellow-400 hover:underline flex items-center" target="_blank" rel="noopener noreferrer">
+              Twitter
+              <ArrowUpRightIcon className="w-4 h-4 ml-1" />
+            </Link>
+            <Link href="https://linkedin.com/in/khanhumayun" className="text-yellow-400 hover:underline flex items-center" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+              <ArrowUpRightIcon className="w-4 h-4 ml-1" />
+            </Link>
+          </div>
+        </section>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'sen': ['var(--font-sen)'],
+        'stolen-hand': ['var(--font-stolen-hand)'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
