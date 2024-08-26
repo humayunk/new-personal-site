542bb86501ab9e4b15d68cfe48905a633497ee6d
humayun khan <humayun.n.k@gmail.com>
2024-08-25 23:38:53 -0400

    fixing mobile issue

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
         <div>
           <p className="mb-8 text-lg">
             Hi, I&apos;m Humayun Khan. I&apos;m a design engineer, startup builder,
-            and writer. I&apos;m currently based in
+            and author. I&apos;m currently based in
             Montreal.
           </p>
 
