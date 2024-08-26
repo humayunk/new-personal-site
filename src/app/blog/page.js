31a761fa7c8bbe486e4aff18ded5059c7e5450d8
humayun khan <humayun.n.k@gmail.com>
2024-08-25 23:34:41 -0400

    fixed something on mobile

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -36,11 +36,11 @@ export default async function BlogPage() {
         <ul className="w-full">
           {posts.map((post) => (
             <li key={post.sys.id} className="mb-8 w-full">
-              <Link href={`/blog/${post.fields.slug}`} className="flex justify-between items-center w-full p-2 rounded">
-                <h2 className="text-lg transition-colors duration-200 hover:text-yellow-400">{post.fields.title}</h2>
-                <div className="flex items-center">
-                  <span className="mr-2 text-gray-400">{post.fields.tags[0]}</span>
-                  <span className="text-gray-400">~ {new Date(post.fields.date).getFullYear()}</span>
+              <Link href={`/blog/${post.fields.slug}`} className="flex flex-col sm:flex-row justify-between items-start sm:items-center w-full p-2 rounded">
+                <h2 className="text-lg transition-colors duration-200 hover:text-yellow-400 mb-2 sm:mb-0">{post.fields.title}</h2>
+                <div className="flex flex-col sm:flex-row items-start sm:items-center text-sm">
+                  <span className="text-gray-400 mb-1 sm:mb-0 sm:mr-2">{post.fields.tags[0]}</span>
+                  <span className="text-gray-400">{new Date(post.fields.date).getFullYear()}</span>
                 </div>
               </Link>
               <div className="border-b border-dotted border-gray-600 mt-4"></div>
